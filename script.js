d95f7fc1c8eb16960ee62b2dd07dea04419fd4a9
Vibhav Misra <99326634+Vibhav-Misra@users.noreply.github.com>
2025-03-21 16:58:05 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("scroll", function () {
-    const hero = document.getElementById("hero");
-    if (window.scrollY > 100) {
-        hero.classList.add("fade-out");
-    } else {
-        hero.classList.remove("fade-out");
-    }
-});
-
